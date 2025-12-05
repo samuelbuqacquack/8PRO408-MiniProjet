407397a56eb06aa7ce36c9758d7a0ab2a1948194
samuelbuqacquack <sekaihenodoa@mutsuba.info>
2025-12-05 17:09:04 -0500

    Add files via upload

--- python/miniprojet.py
+++ python/miniprojet.py
@@ -26,10 +26,10 @@ diagnostic(donnees, "Rapport de pré-nettoyage", "pré-nettoyage.html") # Diagno
 #    ---> Les valeurs manquantes pour chacune de ces colonnes seront nettoyées selon l'analyse nécessaire. 
 # Duplicats: aucun
 # Affichage du type de données
-def affiche_typedonnees(): # On aura besoin de réutiliser.
+def affiche_typedonnees(donnees): # On aura besoin de réutiliser.
     print("-----------------Types de données :")
     print(donnees.dtypes)
-affiche_typedonnees()
+affiche_typedonnees(donnees)
 # --------------------------------------------------------------------------------------------------------------------------------------
 # À part release_year, tous sont identifiés comme "object", donc il faudra normaliser.
 # Affichage d'un aperçu des données
@@ -38,7 +38,7 @@ print(donnees.head())
 # À la vue des données, seules date_aded peuvent être stockées dans une valeur qui n'est pas une string.
 # On essaie avec convert_dtypes()
 donnees = donnees.convert_dtypes()
-affiche_typedonnees()
+affiche_typedonnees(donnees)
 # --------------------------------------------------------------------------------------------------------------------------------------
 # Convertit la date en un format utilisable.
 # Référence: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
@@ -49,18 +49,47 @@ print(donnees.head())
 # Pour ces colonnes, aucune valeur manquante n'est reportée, donc aucun pré-traitement n'est nécessaire.
 # Aperçu des types afin de s'assurer qu'ils ne sont pas dupliqués (majuscules-minuscules)
 # Aperçu également des dates de sortie, afin de s'assurer que les années sont plausibles.
+# (La liste d'année est abrégée mais on peut confirmer l'absence de valeurs aberrantes en la triant)
 types = donnees.groupby("type")["type"].count()
 annees_sortie = donnees.groupby("release_year")["release_year"].count()
 print(types)
-print(annees_sortie)
+print(annees_sortie.sort_values())
 # --------------------------------------------------------------------------------------------------------------------------------------
+# Proportions.
 # Concaténation d'une colonne de pourcentage pour les types, puis affichage
-pourcentages = types / donnees.shape[0] * 100
-types = pd.concat([types.rename("count"),pourcentages.rename("percent")], axis=1)
+types_pourcentages = types / donnees.shape[0] * 100
+types = pd.concat([types.rename("count"),types_pourcentages.rename("percent")], axis=1)
 print(types)
 # --------------------------------------------------------------------------------------------------------------------------------------
-# Tendances par année
-#tendances = donnees.groupby("release_year")["type"].agg({"type":"sum"})
-tendances = donnees.groupby(["release_year","type"])["type"].count()
-print(tendances)
-# TODO, faire un tableau plus compréhensible...
+# Tendances par année.
+# Génère un dataframe à deux index (année, type).
+types_tendances = donnees.groupby(["release_year","type"])["type"].count()
+print(types_tendances)
+print("----------------")
+# Reforme ce dataframe en transformant les types en colonnes
+# (Utilise fillna pour transformer en 0 le compte des années où un type est absent)
+types_tendances = types_tendances.unstack().fillna(0).astype("int64")
+print(types_tendances)
+types_tendances.plot(backend="plotly")
+# --------------------------------------------------------------------------------------------------------------------------------------
+# Genres principaux
+# Pour cette colonne, aucune valeur manquante n'est reportée, donc aucun pré-traitement n'est nécessaire.
+# Aperçu afin de s'assurer qu'ils ne sont pas dupliqués (majuscules-minuscules)
+print(donnees.groupby("listed_in")["listed_in"].count())
+# --------------------------------------------------------------------------------------------------------------------------------------
+# On note la présence de recoupages. La stratégie ici sera de dupliquer 
+#donnees_genre["listed_in"] = donnees_genre["listed_in"].apply(lambda x: pd.DataFrame(x.str))
+#donnees_genre = donnees.groupby("listed_in")["listed_in"].count()
+#print(donnees.groupby("listed_in").apply(lambda x: x["listed_in"].str.split[", "]))
+genres_split = donnees["listed_in"].str.split(pat=", ", expand=True)
+print(genres_split)
+# --------------------------------------------------------------------------------------------------------------------------------------
+# On a obtenu un maximum de trois genres par entrée.
+# Il nous suffit de comptabiliser par chacune des trois colonnes et des les aditionner
+# (On prend soin de remplacer les NaN (genres non comptabilisés dans une colonne) par 0.)
+genres = [genres_split.groupby(0)[0].count(), genres_split.groupby(1)[1].count(), genres_split.groupby(2)[2].count()]
+genres[0] = genres[0].add(genres[1], fill_value=0)
+genres[0] = genres[0].add(genres[2], fill_value=0)
+genres = genres[0].astype("int64")
+print(genres)
+genres.sort_values().plot.bar(backend="plotly") # Affiche un graphique par fréquence
